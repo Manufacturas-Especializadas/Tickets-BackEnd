1eb862ba399257c87b3186e86abfb563db91ef5f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-15 07:58:14 -0600

    Dtos para login, register y para generar el token

---
+++ Tickets/Dtos/RegisterRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Tickets.Dtos
+{
+    public class RegisterRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int PayRollNumber { get; set; }
+
+        public string Password { get; set; } = string.Empty;
+
+        public string RoleName { get; set; } = "Ingeniero";
+    }
+}
---
+++ Tickets/Dtos/TokenResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Tickets.Dtos
+{
+    public class TokenResponseDto
+    {
+        public string AccessToken { get; set; } = string.Empty;
+
+        public string RefreshToken { get; set; } = string.Empty;
+
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
