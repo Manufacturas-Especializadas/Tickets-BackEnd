3cecdca7a42ba7c87e68c2d7bee842c3a29ce6a3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-15 07:57:49 -0600

    Controller con los metodos de logout, login, y register

---
+++ Tickets/Controllers/AuthController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Tickets.Dtos;
+using Tickets.Models;
+using Tickets.Services;
+
+namespace Tickets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        private readonly AppDbContext _context;
+
+        public AuthController(AuthService authService, AppDbContext context)
+        {
+            _authService = authService;
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))
+            {
+                return Unauthorized(new { message = "Usuario no autenticado" });
+            }
+
+            var success = await _authService.LogoutAsync(userId);
+
+            if (!success)
+            {
+                return NotFound(new
+                {
+                    message = "Usuario no encontrado"
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Sesión cerrada correctamente"
+            });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            var response = await _authService.LoginAsync(request);
+
+            if(response == null)
+            {
+                return Unauthorized(new
+                {
+                    message = "Nombre o número de nómina incorrectos"
+                });
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            try
+            {
+                var user = await _authService.RegisterAsync(request);
+
+                if(user == null)
+                {
+                    return BadRequest(new
+                    {
+                        message = "Ya existe un usuario con ese nombre y número de nómina"
+                    });
+                }
+
+                return Ok(new
+                {
+                    message = "Usuario registrado exitosamente"
+                });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new
+                {
+                    message = ex.Message,
+                });
+            }
+        }
+
+        [HttpPost]
+        [Route("Refesh")]
+        public async Task<IActionResult> RefreshToken([FromBody] string refreshToken)
+        {
+            if (string.IsNullOrWhiteSpace(refreshToken))
+            {
+                return BadRequest(new
+                {
+                    message = "El refresh es requerido"
+                });
+            }
+
+            var response = await _authService.RefreshTokenAsync(refreshToken);
+
+            if(response == null)
+            {
+                return Unauthorized(new
+                {
+                    message = "Refresh token inválido o expirado"
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
